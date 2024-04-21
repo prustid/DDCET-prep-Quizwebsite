a83949a4bc964715d88fb06433194f88936cb038
prustid <159414355+prustid@users.noreply.github.com>
2024-04-21 09:02:51 -0700

    Add files via upload

---
+++ questaion.js
@@ -0,0 +1,333 @@
+
+let questions = [
+    {
+        numb: 1,
+        question: "Which of the following is a vector quantity?",
+        answer: "D. Force",
+        options: [
+            "A. Temperature",
+            "B. Mass",
+            "C. Distance",
+            "D. Force"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The energy an object possessess due to its motion is called:",
+        answer: "B. Kinetic energy",
+        options: [
+            "A. Potential energy",
+            "B. Kinetic energy",
+            "C. Elasic energy",
+            "D. Mechanical energy"
+        ]
+    },
+    {
+        numb: 3,
+        question: "If a force of 10 N is applied to an object for 5 seconds, what is the impluse of force?",
+        answer: "B. 50 N s ",
+        options: [
+            "A. 2 N s",
+            "B. 50 N s ",
+            "C. 2 N ",
+            "D. 50 N"
+        ]
+    },
+    {
+        numb: 4,
+        question: "The Linear momentum of an object is defined as:",
+        answer: "B. Mass*Velocity ",
+        options: [
+            "A. Mass*Acceleration ",
+            "B. Mass*Velocity ",
+            "C. Acceleration*Velocity ",
+            "D. Mass/Volume"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The reciprocal of resistance is known as:",
+        answer: "B.Conductance",
+        options: [
+            "A. Impedance ",
+            "B. Conductance ",
+            "C. Capacitance ",
+            "D. Susceptance "
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which temperature scale is an absolute temperature scale?",
+        answer: "C.Kelvin",
+        options: [
+            "A.Celsius ",
+            "B.Fahrenheit ",
+            "C.Kelvin",
+            "D.Rankine"
+        ]
+    },
+    {
+        numb: 7,
+        question: "The SI unit of amplitude is:",
+        answer: "C.Meter",
+        options: [
+            "A.Hertz ",
+            "B.Watt",
+            "C.Meter ",
+            "D.Decibel "
+        ]
+    },
+    {
+        numb: 8,
+        question: "The chemical formula of hydrochloric acid is_________.",
+        answer: "A. HCl",
+        options: [
+            "A. HCl",
+            "B. H2SO4",
+            "C. HNO3",
+            "D. CH3COOH"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which of the following is an example of strong base?",
+        answer: "D. NaOH ",
+        options: [
+            "A. NH4OH",
+            "B.Ca(OH)2",
+            "C. Both A and B ",
+            "D. NaOH "
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which of the following is an example of weak acid?",
+        answer: "B. Acetic acid",
+        options: [
+            "A.Hydrochrolic acid ",
+            "B.Acetic acid",
+            "C.Sulphuric acid",
+            "D.Nitric acid "
+        ]
+    },
+    {
+        numb: 11,
+        question: "Which gas is produced by the reaction of acid with metal?",
+        answer: "A. Hydrogen",
+        options: [
+            "A.Hydrogen ",
+            "B.Oxygen",
+            "C.Carbon dioxide ",
+            "D.Nitrogen "
+        ]
+    },
+    {
+        numb: 12,
+        question: "Gardens are example of ",
+        answer: " B. Artificial ecosystem",
+        options: [
+            "A. Natural ecosystem",
+            "B. Artificial ecosystem",
+            "C. Ecology",
+            "D. Environment"
+        ]
+    },
+    {
+        numb: 13,
+        question: "Whice of the following is a non-renewable energy resource?",
+        answer: "D. Coal ",
+        options: [
+            "A. Solar",
+            "B. Methane",
+            "C. Hydroelectric",
+            "D. Coal"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What is the smallest unit of information?",
+        answer: "B. Bit ",
+        options: [
+            "A. Byte",
+            "B. Bit",
+            "C. MB",
+            "D. GB"
+        ]
+    },
+    {
+        numb: 15,
+        question: "What is the default extension of a microsoft excel file?",
+        answer: "C. xlsx ",
+        options: [
+            "A. msxcl",
+            "B. xcl",
+            "C. xlsx",
+            "D. xlsm"
+        ]
+    },
+    {
+        numb: 16,
+        question: "Rima is a good dancer. The underlined word “good” in the sentence given is________.",
+        answer: "B. an adjective",
+        options: [
+            "A. a noun",
+            "B. an adjective",
+            "C. a verb",
+            "D. an adverb"
+        ]
+    },
+    {
+        numb: 17,
+        question: "___________ you work hard, you will succeed in the university Examination.",
+        answer: "D. If ",
+        options: [
+            "A. Though",
+            "B. But",
+            "C. Unless",
+            "D. If"
+        ]
+    },
+    {
+        numb: 18,
+        question: "One of my friends __________ old.",
+        answer: "B. is ",
+        options: [
+            "A. am",
+            "B. is",
+            "C. are",
+            "D. has"
+        ]
+    },
+    {
+        numb: 19,
+        question: "Choose the correct spelling.",
+        answer: "B. Vacuum",
+        options: [
+            "A. Vaccum",
+            "B. Vacuum",
+            "C. Vacumm",
+            "D. Veccum"
+        ]
+    },
+    {
+        numb: 20,
+        question: "Choose the correct spelling.",
+        answer: "C. Entrepreneur",
+        options: [
+            "A. Enterpreneur",
+            "B. Entreorenure",
+            "C. Entrepreneur",
+            "D. Enterprenure"
+        ]
+    },
+    {
+        numb: 21,
+        question: "Choose the correct sentence.",
+        answer: "D.He is reading a book now.",
+        options: [
+            "A.He is reading a book yesterday. ",
+            "B.He reads a book now.",
+            "C.He read a book now.",
+            "D.He is reading a book now."
+        ]
+    },
+    {
+        numb: 22,
+        question: "Choose the correct sentence.",
+        answer: "A. I don’t use public transportdaily.",
+        options: [
+            "A. I don’t use public transportdaily.",
+            "B. I didn’t use public transport daily.",
+            "C. I not use public transport daily.",
+            "D. I not using public transport daily."
+        ]
+    },
+    {
+        numb: 23,
+        question: "The person who transmit the message is called what?",
+        answer: "B. Sender",
+        options: [
+            "A. Channel",
+            "B. Sender",
+            "C. Receiver",
+            "D. Respondent"
+        ]
+    },
+    {
+        numb: 24,
+        question: "If the order of the matrix A is 4*3 orde of matrix B is 3*5 and the order of matri ABC is 4*2 then the order of matrix C is :",
+        answer: "D. 5*2",
+        options: [
+            "A. 4*2",
+            "B. 3*2",
+            "C. 2*2",
+            "D. 5*2"
+        ]
+    },
+    {
+        numb: 25,
+        question: "For square matrix A if 1/A=adjA then |A|= ?",
+        answer: "A. 1",
+        options: [
+            "A. 1",
+            "B. 0",
+            "C. -1",
+            "D. None of above"
+        ]
+    },
+    {
+        numb: 26,
+        question: "If vectors (m,3,5) and (m,m-1,-3) are perpendicular to each other then m=_____?",
+        answer: "B. 0",
+        options: [
+            "A. 3",
+            "B. 0",
+            "C. 2",
+            "D. 6"
+        ]
+    },
+    {
+        numb: 27,
+        question: "Equation of line perpendicular to line 3x-2y+7=0 and passess through (3,2) is ___",
+        answer: "B. 2x+3y-5=0",
+        options: [
+            "A. 3x+2y-11=0",
+            "B. 2x+3y-5=0",
+            "C. 2x+3y+12=0",
+            "D. 2x+3y-12=0"
+        ]
+    },
+    {
+        numb: 28,
+        question: "If x=a(1+sin0),y=a(1-cos0) then dy/dx =",
+        answer: "D. sec 0",
+        options: [
+            "A. cot 0",
+            "B. sin 0",
+            "C. tan 0",
+            "D. sec 0"
+        ]
+    },
+    {
+        numb: 29,
+        question: "sin2A+sin8A/cos2A+cos8A =____",
+        answer: "C. tan 5A",
+        options: [
+            "A. cot 5A",
+            "B. tan 10A",
+            "C. tan 5A",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 30,
+        question: "If mean of 3,5,a,4,6 is 5 then a=___",
+        answer: "C. 7",
+        options: [
+            "A. 5",
+            "B. 6",
+            "C. 7",
+            "D. 8"
+        ]
+    }
+];
